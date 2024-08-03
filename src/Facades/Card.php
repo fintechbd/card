6022cb18601a8de8ce41aa620bd748c20d84a8ce
hafijul233 <hafijul233@users.noreply.github.com>
2024-08-03 20:47:16 +0000

    Fix styling

--- src/Facades/Card.php
+++ src/Facades/Card.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \Fintech\Card\Services\InstantCardService instantCard()
- * // Crud Service Method Point Do not Remove //
+ *                                                                        // Crud Service Method Point Do not Remove //
  *
  * @see \Fintech\Card\Card
  */
