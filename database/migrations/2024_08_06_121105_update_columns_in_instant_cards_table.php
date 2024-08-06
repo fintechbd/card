c7298fa27de77a81324151f6b930540f7b0230ba
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-06 18:15:53 +0600

    migration added

---
+++ database/migrations/2024_08_06_121105_update_columns_in_instant_cards_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('instant_cards', function (Blueprint $table) {
+            $table->json('timeline')->nullable()->after('instant_card_data');
+            $table->string('block_reason')->nullable()->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('instant_cards', function (Blueprint $table) {
+            $table->dropColumn('timeline');
+            $table->dropColumn('block_reason');
+        });
+    }
+};
