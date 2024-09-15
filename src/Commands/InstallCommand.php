0bf7ac2a078f21b04b699e20c2eed06789abda19
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-16 02:44:31 +0600

    installer optimized

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -2,17 +2,26 @@
 
 namespace Fintech\Card\Commands;
 
+use Fintech\Core\Traits\HasCoreSettingTrait;
 use Illuminate\Console\Command;
 
 class InstallCommand extends Command
 {
+    use HasCoreSettingTrait;
+
     public $signature = 'card:install';
 
-    public $description = 'My command';
+    public $description = 'Configure the system for the `fintech/card` module';
+
+    private string $module = 'Card';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        $this->infoMessage("Module Installation", 'RUNNING');
+
+        $this->task("Module Installation", function () {
+
+        }, 'COMPLETED');
 
         return self::SUCCESS;
     }
