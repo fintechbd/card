b9a0c7f05660d0f3dad5165b0400aadff6537147
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-02-24 15:05:47 +0600

    not found merged with failed exception response

--- src/Http/Controllers/PrepaidCardController.php
+++ src/Http/Controllers/PrepaidCardController.php
@@ -104,11 +104,7 @@ class PrepaidCardController extends Controller
 
             return new PrepaidCardResource($prepaidCard);
 
-        } catch (ModelNotFoundException $exception) {
-
-            return response()->notfound($exception->getMessage());
-
-        } catch (Exception $exception) {
+        }  catch (Exception $exception) {
 
             return response()->failed($exception);
         }
@@ -142,11 +138,7 @@ class PrepaidCardController extends Controller
 
             return response()->updated(__('core::messages.resource.updated', ['model' => 'Prepaid Card']));
 
-        } catch (ModelNotFoundException $exception) {
-
-            return response()->notfound($exception->getMessage());
-
-        } catch (Exception $exception) {
+        }  catch (Exception $exception) {
 
             return response()->failed($exception);
         }
@@ -180,11 +172,7 @@ class PrepaidCardController extends Controller
 
             return response()->deleted(__('core::messages.resource.deleted', ['model' => 'Prepaid Card']));
 
-        } catch (ModelNotFoundException $exception) {
-
-            return response()->notfound($exception->getMessage());
-
-        } catch (Exception $exception) {
+        }  catch (Exception $exception) {
 
             return response()->failed($exception);
         }
@@ -216,11 +204,7 @@ class PrepaidCardController extends Controller
 
             return response()->restored(__('core::messages.resource.restored', ['model' => 'Prepaid Card']));
 
-        } catch (ModelNotFoundException $exception) {
-
-            return response()->notfound($exception->getMessage());
-
-        } catch (Exception $exception) {
+        }  catch (Exception $exception) {
 
             return response()->failed($exception);
         }
@@ -300,11 +284,7 @@ class PrepaidCardController extends Controller
 
             return response()->updated(__('core::messages.resource.updated', ['model' => 'Prepaid Card']));
 
-        } catch (ModelNotFoundException $exception) {
-
-            return response()->notfound($exception->getMessage());
-
-        } catch (Exception $exception) {
+        }  catch (Exception $exception) {
 
             return response()->failed($exception);
         }
